596076818150b7cba0707e1479e15a44a842d22a
LauraJohnston20 <95102375+LauraJohnston20@users.noreply.github.com>
2022-04-14 12:11:06 -0230

    Final Upload

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-$("#volumeConversion").conversionPopup();
+$("#volumeConversion").conversionPopup({
+    overlay: 'rgba(0, 0, 0, 0.5)',
+    closeButton: {
+        src: null,
+        width: "20px",
+        height: "30px"
+    },
+    conversionDisplay: {
+        background: 'rgba(200, 200, 200, 1)',
+        border: "5px solid #ffffff",
+        borderRadius: "15px",
+        fontSize: "large",
+        fontFamily: "sans-serif",
+        textAlign: "center", 
+        textColor: "black"
+    },
+    open: function() {
+        console.log("opened")
+    },
+    close: function() {
+        console.log("closed")
+    }
+ });
+ 
